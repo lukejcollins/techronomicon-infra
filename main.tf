57f9664214590f9bf86266e7a5eedee312eb260c
Luke Collins <luke.collins@live.co.uk>
2023-07-08 00:25:15 +0100

    Enable DNS on VPC

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ terraform {
 # Definition of an AWS VPC resource
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = "my_vpc"
