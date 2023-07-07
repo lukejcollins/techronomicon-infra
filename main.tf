8aa21182a012bc13ea061d07dd159b6b29ed00fa
Luke Collins <luke.collins@live.co.uk>
2023-07-07 17:15:39 +0100

    Remove elastic ip and nat gateway

--- main.tf
+++ main.tf
@@ -28,13 +28,3 @@ resource "aws_internet_gateway" "my_igw" {
     Name = "my_igw"
   }
 }
-
-resource "aws_eip" "my_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "my_nat_gateway" {
-  allocation_id = aws_eip.my_eip.id
-  subnet_id     = aws_subnet.my_public_subnet.id
-}
-
