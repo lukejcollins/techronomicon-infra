cb84bc34f7b2602db8b85beeb540e454d8b06b08
Luke Collins <luke.collins@live.co.uk>
2023-07-07 17:29:15 +0100

    It should be public, not private

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-resource "aws_subnet" "my_private_subnet" {
+resource "aws_subnet" "my_public_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
 
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "my_private_subnet"
+    Name = "my_public_subnet"
   }
 }
 
