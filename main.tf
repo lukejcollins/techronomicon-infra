41bc06c16a3211e66d8dc800ca1eb41357f482fb
Luke Collins <luke.collins@live.co.uk>
2023-07-09 22:23:37 +0100

    Pass y parameter to dnf install ecs-init

--- main.tf
+++ main.tf
@@ -281,7 +281,7 @@ resource "aws_instance" "example" {
     #!/bin/bash
     mkdir -p /etc/ecs
     echo "ECS_CLUSTER=techronomicon-cluster" > /etc/ecs/ecs.config
-    dnf install ecs-init
+    dnf install ecs-init -y
     systemctl enable --now ecs
   EOF
 
