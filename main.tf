86b70e6b893560d721466d0b5a5a04a012abc3de
Luke <44213313+lukejcollins@users.noreply.github.com>
2023-07-24 16:12:01 +0100

    Separate route53 (#35)
    * Move route53 config to new file

--- main.tf
+++ main.tf
@@ -450,17 +450,3 @@ resource "aws_ecs_service" "service" {
   desired_count   = 1
   launch_type     = "EC2"
 }
-
-# Create an AWS Route 53 Zone for the domain "lukecollins.dev"
-resource "aws_route53_zone" "my_domain" {
-  name = "lukecollins.dev"
-}
-
-# Create a Route 53 record for the domain "lukecollins.dev"
-resource "aws_route53_record" "my_domain_a" {
-  zone_id = aws_route53_zone.my_domain.zone_id
-  name    = "lukecollins.dev"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.example.public_ip]
-}
---
+++ route53.tf
@@ -0,0 +1,13 @@
+# Create an AWS Route 53 Zone for the domain "lukecollins.dev"
+resource "aws_route53_zone" "my_domain" {
+  name = "lukecollins.dev"
+}
+
+# Create a Route 53 record for the domain "lukecollins.dev"
+resource "aws_route53_record" "my_domain_a" {
+  zone_id = aws_route53_zone.my_domain.zone_id
+  name    = "lukecollins.dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.example.public_ip]
+}
